f90bb4672624fd1941917b44b58ee0fabdef8689
varnikgen <varnikgen@yandex.ru>
2021-01-19 15:45:25 +1000

    Django менеджер и шаблоны (стрёмный способ выводить категории)

--- shop/mainapp/models.py
+++ shop/mainapp/models.py
@@ -7,6 +7,10 @@ from django.urls import reverse
 User = get_user_model()
 
 
+def get_models_for_count(*model_names):
+    return [models.Count(model_name) for model_name in model_names]
+
+
 def get_product_url(obj, viewname):
     ct_model = obj.__class__._meta.model_name
     return reverse(viewname, kwargs={'ct_model': ct_model, 'slug': obj.slug})
@@ -53,12 +57,33 @@ class LatestProducts:
     objects = LatestProductsManager()
 
 
+class CategoryManager(models.Manager):
+    """
+    Вывод товаров по категории
+    """
+    CATEGORY_NAME_COUNT_NAME = {
+        'Ванны': 'bath__count',
+        'Смесители': 'mixer__count',
+    }
+
+    def get_queryset(self):
+        return super().get_queryset()
+    
+    def get_categories_for_left_sidebar(self):
+        """Получить список категорий для сайтбара"""
+        models = get_models_for_count('bath', 'mixer')
+        qs = list(self.get_queryset().annotate(*models).values())
+        return [dict(name=c['name'], slug=c['slug'], count=c[self.CATEGORY_NAME_COUNT_NAME[c['name']]]) for c in qs]
+
+
+
 class Category(models.Model):
     """
     Модель категории тваров
     """
     name = models.CharField(max_length=255, verbose_name='Имя категории')
     slug = models.SlugField(unique=True)
+    objects = CategoryManager()
 
     def __str__(self):
         return self.name
@@ -68,10 +93,6 @@ class Product(models.Model):
     """
     Модель товара
     """
-
-    MIN_RESOLUTION = (300, 300)
-    MAX_RESOLUTION = (800, 800)
-    MAX_IMAGE_SIZE = 3145728
 
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
