69f0be238b2717d6504fed663ef62a67d07536ed
Nikolay Varfolomeev <varnikgen@yandex.ru>
2021-01-20 13:52:19 +1000

    Миксины

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,15 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic import DetailView, View
 
 from .models import Bath, Mixer, Category
+from .mixins import CategoryDetailMixin
 
 
-def test_view(request):
-    categories = Category.objects.get_categories_for_left_sidebar()
-    return render(request, 'base.html', {'categories': categories})
+class BaseView(View):
+    """
+    Базовая View
+    """
+    def get(self, request, *args, **kwargs):
+        categories = Category.objects.get_categories_for_left_sidebar()
+        return render(request, 'base.html', {'categories': categories})
 
 
-class ProductDetailView(DetailView):
+# def test_view(request):
+#     categories = Category.objects.get_categories_for_left_sidebar()
+#     return render(request, 'base.html', {'categories': categories})
+
+
+class ProductDetailView(CategoryDetailMixin, DetailView):
     """
     Класс для представления всех продуктовых классов из моделей в одном шаблоне
     """
@@ -28,7 +38,7 @@ class ProductDetailView(DetailView):
     slug_url_kwarg = 'slug'
 
 
-class CategoryDetailView(DetailView):
+class CategoryDetailView(CategoryDetailMixin, DetailView):
     """
     Представление категирии товара
     """
