cb527900be0fe063ee79be2030619f59525e19f1
varnikgen <varnikgen@yandex.ru>
2021-01-21 16:25:36 +1000

    Добавление товара в корзину

--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -1,7 +1,9 @@
+from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic import DetailView, View
 
-from .models import Bath, Mixer, Category, LatestProducts, Customer, Cart
+from .models import Bath, Mixer, Category, LatestProducts, Customer, Cart, CartProduct
 from .mixins import CategoryDetailMixin
 
 
@@ -14,9 +16,12 @@ class BaseView(View):
         products = LatestProducts.objects.get_products_for_main_page(
             'bath', 'mixer', with_respect_to='mixer'
         )
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer)
         context = {
             'categories': categories,
             'products': products,
+            'cart': cart,
         }
         return render(request, 'base.html', context)
 
@@ -39,16 +44,40 @@ class ProductDetailView(CategoryDetailMixin, DetailView):
     template_name = 'product_detail.html'
     slug_url_kwarg = 'slug'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["ct_model"] = self.model._meta.model_name
+        return context
+    
+
 
 class CategoryDetailView(CategoryDetailMixin, DetailView):
     """
-    Представление категирии товара
+    Представление категории товара
     """
     model = Category
     queryset = Category.objects.all()
     context_object_name = 'category'
     template_name = 'category_detail.html'
     slug_url_kwarg = 'slug'
+
+
+class AddToCartView(View):
+    """
+    Добавление товара в корзину
+    """
+    def get(self, request, *args, **kwargs):
+        ct_model, product_slug = kwargs.get('ct_model'), kwargs.get('slug')
+        customer = Customer.objects.get(user=request.user)
+        cart = Cart.objects.get(owner=customer, in_order=False)
+        content_type = ContentType.objects.get(model=ct_model)
+        product = content_type.model_class().objects.get(slug=product_slug)
+        cart_product, created = CartProduct.objects.get_or_create(
+            user=cart.owner, cart=cart, content_type=content_type, object_id=product.id, final_price=product.price
+
+        )
+        cart.products.add(cart_product)
+        return HttpResponseRedirect('/cart/')
 
 
 class CartView(View):
