ec1e84ab7e2e0afdcb6a2657865c4a50ae6a0021
varnikgen <varnikgen@yandex.ru>
2021-02-05 16:26:04 +1000

    удалил REST API за ненадобностью, на данный момент

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -21,7 +21,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('mainapp.api.urls')),
     path('', include('mainapp.urls')),
 ]
 
