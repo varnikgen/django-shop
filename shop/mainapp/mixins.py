4a1e07a65c5d11823ba53d89a66724f2e4623e45
varnikgen <varnikgen@yandex.ru>
2021-02-08 16:33:01 +1000

    Расширени navbar

--- shop/mainapp/mixins.py
+++ shop/mainapp/mixins.py
@@ -16,8 +16,8 @@ class CartMixin(View):
             if not cart:
                 cart = Cart.objects.create(owner=customer)
         else:
-            cart = Cart.objects.filter(for_anonimus_user=True).first()
+            cart = Cart.objects.filter(for_anonymous_user=True).first()
             if not cart:
-                cart = Cart.objects.create(for_anonimus_user=True)
+                cart = Cart.objects.create(for_anonymous_user=True)
         self.cart = cart
         return super().dispatch(request, *args, **kwargs)
