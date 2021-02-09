ef86219a471c28f5d2ac0ec93acfaf9f02ecfb18
varnikgen <varnikgen@yandex.ru>
2021-02-09 16:34:25 +1000

    Регистрация

--- shop/mainapp/forms.py
+++ shop/mainapp/forms.py
@@ -44,3 +44,58 @@ class LoginForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'password']
+
+
+
+class RegistrationForm(forms.ModelForm):
+    """
+    Класс форм регистрации пользователя
+    """
+    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    password = forms.CharField(widget=forms.PasswordInput)
+    phone = forms.CharField(required=True)
+    address = forms.CharField(required=False)
+    email = forms.EmailField(required=True)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Логин'
+        self.fields['password'].label = 'Пароль'
+        self.fields['confirm_password'].label = 'Подтвердите пароль'
+        self.fields['phone'].label = 'Номер телефона'
+        self.fields['first_name'].label = 'Ваше имя'
+        self.fields['last_name'].label = 'Ваша фамилия'
+        self.fields['address'].label = 'Адрес'
+        self.fields['email'].label = 'Электронная почта'
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        domain = email.split('.')[-1]
+        if domain in ['com', 'net']:
+            raise forms.ValidationError(f'Регистрация для "{domain}" невозможна')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError(f'Данный адрес "{email}" уже зарегистрирован в системе')
+        return email
+    
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        if User.objects.filter(username=username).exists():
+            raise forms.ValidationError(f'Имя пользователя {username} уже занято')
+        return username
+    
+    def clean(self):
+        password = self.cleaned_data['password']
+        confirm_password = self.cleaned_data['confirm_password']
+        first_name = self.cleaned_data['first_name']
+        last_name = self.cleaned_data['last_name']
+        if password != confirm_password:
+            raise forms.ValidationError('Пароли не совпадают')
+        if not first_name:
+            self.cleaned_data['first_name'] = self.cleaned_data['username']
+        if not last_name:
+            self.cleaned_data['last_name'] = self.cleaned_data['username']
+        return self.cleaned_data
+
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'confirm_password', 'first_name', 'last_name', 'address', 'phone', 'email']
--- shop/mainapp/views.py
+++ shop/mainapp/views.py
@@ -8,7 +8,7 @@ from django.views.generic import DetailView, View
 
 from .models import Category, Customer, Cart, CartProduct, Product
 from .mixins import CartMixin
-from .forms import OrderForm, LoginForm
+from .forms import OrderForm, LoginForm, RegistrationForm
 from .utils import recalc_cart
 
 
@@ -184,4 +184,39 @@ class LoginView(CartMixin, View):
             if user:
                 login(request, user)
                 return HttpResponseRedirect('/')
-        return render(request, 'login.html', {'form': form, 'cart': self.cart})
+        context = {'form': form, 'categories': categories, 'cart': self.cart}
+        return render(request, 'login.html', context)
+
+
+class RegistrationView(CartMixin, View):
+    """
+    Отображаем форму регистрации
+    """
+    def get(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        categories = Category.objects.all()
+        context = {'form': form, 'categories': categories, 'cart': self.cart}
+        return render(request, 'registration.html', context)
+    
+    def post(self, request, *args, **kwargs):
+        form = RegistrationForm(request.POST or None)
+        if form.is_valid():
+            new_user = form.save(commit=False)
+            new_user.username = form.cleaned_data['username']
+            new_user.email = form.cleaned_data['email']
+            new_user.first_name = form.cleaned_data['first_name']
+            new_user.last_name = form.cleaned_data['last_name']
+            new_user.save()
+            new_user.set_password(form.cleaned_data['password'])
+            new_user.save()
+            Customer.objects.create(
+                user=new_user,
+                phone=form.cleaned_data['phone'],
+                address=form.cleaned_data['address'],
+            )
+            user = authenticate(username=form.cleaned_data['username'], password=form.cleaned_data['password'])
+            login(request, user)
+            return HttpResponseRedirect('/')
+        categories = Category.objects.all()
+        context = {'form': form, 'categories': categories, 'cart': self.cart}
+        return render(request, 'registration.html', context)
